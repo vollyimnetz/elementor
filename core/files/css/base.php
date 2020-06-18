6b57801af19f10ee257bd1d00ca602709c6bc318
Mati Horovitz <mati@elementor.com>
2020-06-18 21:53:15 +0300

    CSS Parser/PHP: Fix  - Responsive settings/variables not working.

--- core/files/css/base.php
+++ core/files/css/base.php
@@ -315,7 +315,8 @@ abstract class Base extends Base_File {
 				$id = $global_args[1];
 
 				if ( ! empty( $control['groupType'] ) ) {
-					$property_name = str_replace( [ $control['groupPrefix'], '_' ], [ '', '-' ], $control['name'] );
+					$property_name = str_replace( [ $control['groupPrefix'], '_tablet', '_mobile' ], '', $control['name'] );
+					$property_name = str_replace( '_', '-', $property_name );
 
 					$property_value = "var( --e-global-$control[groupType]-$id-$property_name )";
 				} else {
